4af6dff28bf420a1bb0fb00b074b97a07aeed66f
Habeebhabs <Habeebhabs123@gmail.com>
2022-04-06 23:45:58 +0530

    firebase related

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'my-firebase-app-2fece',
+    appId: '1:794750601783:web:a03936ebc1ca1749278db2',
+    storageBucket: 'my-firebase-app-2fece.appspot.com',
+    locationId: 'asia-south1',
+    apiKey: 'AIzaSyD-vz5We5ptMGI53UW-KeWNCIZtYqH7olI',
+    authDomain: 'my-firebase-app-2fece.firebaseapp.com',
+    messagingSenderId: '794750601783',
+  },
   production: false
 };
 
